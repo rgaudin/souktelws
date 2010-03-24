c418e8ed4c23f910be0975f8dfce22f61c15130d
reg <reg@devsms.(none)>
2010-03-24 09:53:17 +0200

    Added keyworder

--- apps/renaud/app.py
+++ apps/renaud/app.py
@@ -3,13 +3,36 @@
 # maintainer: rgaudin
 
 import rapidsms
+from rapidsms.parsers.keyworder import Keyworder
 
 class App(rapidsms.app.App):
 
+    keyword = Keyworder()
+
     def handle(self, message):
 
+        try:
+            func, captures = self.keyword.match(self, message.text)
+        except TypeError:
+            #message.respond(u"Unrecognised message")
+            return False
+        try:
+            return func(self, message, *captures)
+        except Exception, e:
+            message.respond(u"System encountered an Error: %s" % e)
+            return True
+
+    def old_handle(self, message):
+        ''' not used anymore '''
         if message.text.lower().startswith('renaud'):
             message.respond(u"Hello %s" % message.text)
             return True
 
         return False
+
+    keyword.prefix = ['renaud']
+    @keyword(r'(\w+)')
+    def renaud(self, message, text):
+        message.respond(u"Hello %s. Thank you" % text)
+        return True
+
