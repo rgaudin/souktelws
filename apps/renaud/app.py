620e795ea87d1019ca7f92ed34690c18fcc80dd3
reg <reg@devsms.(none)>
2010-03-24 10:59:29 +0200

    added another keyword

--- apps/renaud/app.py
+++ apps/renaud/app.py
@@ -31,8 +31,21 @@ class App(rapidsms.app.App):
         return False
 
     keyword.prefix = ['renaud']
-    @keyword(r'(\w+)')
-    def renaud(self, message, text):
-        message.respond(u"Hello %s. Thank you" % text)
+    @keyword(r'(\w+) ([y|n])')
+    def renaud(self, message, text, exist):
+        if exist == 'y':
+            message.respond(u"Yes, %s exists." % text)
+        else:
+            message.respond(u"Sorry, %s doesn't exist." % text)
+        return True
+
+    keyword.prefix = ['product','multiply']
+    @keyword(r'(\d+) (\d+)')
+    def multiplication(self, message, first, second):
+        first = int(first)
+        second = int(second)
+        result = first * second
+
+        message.respond(u"%(first)s * %(second)s = %(res)s" % {'first': first, 'second': second, 'res': result})
         return True
 
