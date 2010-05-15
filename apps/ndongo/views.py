e2cf6b2e0d262093d3022532205964a26c781ca2
dev <dev@rapid-server.(none)>
2010-05-15 16:12:26 +0300

    updated ui

--- apps/ndongo/views.py
+++ apps/ndongo/views.py
@@ -11,15 +11,25 @@ from models import *
 @login_required
 def index(request):
 
-    # Group A & B
-    groups = Group.objects.filter(name__in=('A','B'))
+    if request.method == 'POST':
+        try:
+            nb_winners = int(request.POST['nb_winners'])
+        except ValueError:
+            nb_winners = 0
+    else:
+        nb_winners = 0
 
-    # list of students from those groups
-    students = Attendee.objects.filter(group__in=(groups))
+    if nb_winners:
+        # Group A & B
+        groups = Group.objects.filter(name__in=('A','B'))
 
-    # random 10 students
-    winners = random.sample(students, 10)
-    
+        # list of students from those groups
+        students = Attendee.objects.filter(group__in=(groups))
+
+        # random 10 students
+        winners = random.sample(students, nb_winners)
+    else:
+        winners = None
 
     return render_to_response(request, 'index.html', {'winners': winners})
 
